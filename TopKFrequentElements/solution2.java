a93eee903d4fd5290078b6937bf7913ca377ffda
Richard Chao <richardchao2009@gmail.com>
2023-10-09 17:46:25 +0800

    add time complexity in comments

--- TopKFrequentElements/solution2.java
+++ TopKFrequentElements/solution2.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+// TC: O(nlog(k))
+// SC: O(n)
 public class solution2 {
     public static int[] topKFrequent(int[] nums, int k) {
         int[] arr = new int[k];
