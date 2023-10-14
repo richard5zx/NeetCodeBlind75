c8790189e963b3c636b4949b323d0011e83a158c
Richard Chao <richardchao2009@gmail.com>
2023-10-14 11:28:11 +0800

    Print binary representation by loop

---
+++ Numberof1Bits/prac1.java
@@ -0,0 +1,29 @@
+package Numberof1Bits;
+
+import java.util.Scanner;
+
+public class prac1 {
+    // Print binary representation of an integer
+    public static void main(String args[]) {
+
+        System.out.print("Enter Integer: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+
+        int originalMask = 1;
+        int numberOfBits = 32;
+        int mask;
+        
+        System.out.print("Binary representation of " + input + " is: ");
+        for (int i = 1; i <= numberOfBits; i++) {
+            mask = originalMask << numberOfBits - i; 
+            if ((mask & input) != 0) {
+                System.out.print(1);
+            } else {
+                System.out.print(0);
+            }
+    
+        }
+    }
+}
