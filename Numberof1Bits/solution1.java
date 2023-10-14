32204c08f12ae85b693d1007677423b58acc76cf
Richard Chao <richardchao2009@gmail.com>
2023-10-14 11:01:41 +0800

    solution complete

---
+++ Numberof1Bits/solution1.java
@@ -0,0 +1,33 @@
+package Numberof1Bits;
+
+public class solution1 {
+    public static int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask = mask << 1;
+        }
+        return bits;
+    }
+
+    public static void main(String args[]) {
+        // Binary Representation: 00000000000000000000000000001011
+        // Decimal Representation: 11
+        int input1 = 11;
+        System.out.println(hammingWeight(input1));
+
+        // Binary Representation: 00000000000000000000000010000000
+        // Decimal Representation: 128
+        int input2 = 128;
+        System.out.println(hammingWeight(input2));
+
+        // Binary Representation: 11111111111111111111111111111101
+        // Decimal Representation: -3
+        int input3 = -3;
+        System.out.println(hammingWeight(input3));
+    }
+}
