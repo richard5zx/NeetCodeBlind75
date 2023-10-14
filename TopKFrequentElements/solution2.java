f66a937f01a3963ea858d41f6f400c50d0216b2a
Richard Chao <richardchao2009@gmail.com>
2023-10-14 10:09:01 +0800

    solution 2  top k frequent

--- TopKFrequentElements/solution2.java
+++ TopKFrequentElements/solution2.java
@@ -8,22 +8,23 @@ import java.util.PriorityQueue;
 // SC: O(n)
 public class solution2 {
     public static int[] topKFrequent(int[] nums, int k) {
-        int[] arr = new int[k];
+        int[] result = new int[k];
+
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
-                (a, b) ->
-            a.getValue() - b.getValue()
-        );
+
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         for (Map.Entry<Integer, Integer> it : map.entrySet()) {
             pq.add(it);
             if (pq.size() > k) pq.poll();
         }
+
         int i = k;
         while (!pq.isEmpty()) {
-            arr[--i] = pq.poll().getKey();
+            result[--i] = pq.poll().getKey();
         }
-        return arr;
+
+        return result;
     }
 
     public static void main(String[] args) {
